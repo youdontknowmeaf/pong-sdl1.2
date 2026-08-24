48ea1081c888eefe48e29317b9381044c8fa3a5b
youdontknowmeaf <core.contact@yahoo.com>
2026-08-24 17:23:31 +0200

    Cleanup Refactored the files Clarified PSVita port's compatibility Tried to do shit with Wii port but went nowhere.

--- wii/new.Makefile.mk
+++ wii/new.Makefile.mk
@@ -33,7 +33,7 @@ LDFLAGS	=	-g $(MACHDEP) -Wl,-Map,$(notdir $@).map
 #---------------------------------------------------------------------------------
 # any extra libraries we wish to link with the project
 #---------------------------------------------------------------------------------
-#LIBS	:=	-lSDL -lpngu `$(PREFIX)pkg-config freetype2 libpng libjpeg --libs` -lfat -lwiiuse -lbte -logc -lm
+LIBS    := -lSDL -lSDL_image -lSDL_mixer -lSDL_ttf -lSDL_gfx -lfreetype -ljpeg -lpng -lz -lwiiuse -lbte -lwiikeyboard -lfat -logc -lm
 
 #---------------------------------------------------------------------------------
 # list of directories containing libraries, this must be the top level containing
